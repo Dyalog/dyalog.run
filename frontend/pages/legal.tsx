61ee8a22a72bbbbdde32f2932122cbec7974dd96
Jason Rivers <jason@jasonrivers.co.uk>
2022-09-13 10:30:41 +0100

    Fix text formatting

--- frontend/pages/legal.tsx
+++ frontend/pages/legal.tsx
@@ -20,30 +20,22 @@ export default function Home() {
               </a>
             </aside>
             <p className="text-justify mb-3">
-              dyalog.run is a
-              <a href="https://en.wikipedia.org/wiki/Fork_(software_development)">fork</a>
-              of
-              <a href="https://ato.pxeger.com/">Attempt This Online</a>
-              (<a href="https://github.com/attempt-this-online/attempt-this-online">GitHub</a>)
-              which is
-              <a href="https://github.com/attempt-this-online/attempt-this-online#licence">
-              © 2022 Patrick Reader and contributor
-              </a>.
-              The software is used in accordance with its licence, which is
-              <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">
-              GNU Affero General Public License 3.0
-              </a>,
-              and is provided without warranty. dyalog.run is hosted by
-              <a href="https://www.dyalog.com/">Dyalog Ltd</a>
-              at
-              <a href="https://dyalog.run">https://dyalog.run</a>
-              and its instance of the software repository is hosted at
-              <a href="https://github.com/Dyalog/dyalog.run">https://github.com/Dyalog/dyalog.run</a>.
+              dyalog.run is a <a href="https://en.wikipedia.org/wiki/Fork_(software_development)"
+              className="underline text-blue-500">fork</a> of <a href="https://ato.pxeger.com/">Attempt This
+              Online</a> (<a href="https://github.com/attempt-this-online/attempt-this-online"
+              className="underline text-blue-500">GitHub</a>) which is <a href="https://github.com/attempt-this-online/attempt-this-online#licence"
+              className="underline text-blue-500">© 2022 Patrick Reader and contributor</a>. The software is
+              used in accordance with its licence, which is <a href="https://www.gnu.org/licenses/agpl-3.0.en.html"
+              className="underline text-blue-500">GNU Affero General Public License 3.0</a>, and is provided without warranty.
+              dyalog.run is hosted by <a href="https://www.dyalog.com/" className="underline text-blue-500">Dyalog
+              Ltd</a> at <a href="https://dyalog.run" className="underline text-blue-500">https://dyalog.run</a> and its
+              instance of the software repository is hosted at <a href="https://github.com/Dyalog/dyalog.run"
+              className="underline text-blue-500">https://github.com/Dyalog/dyalog.run</a>.
             </p>
             <p className="text-justify mb-3">
               The principal purpose of dyalog.run is to support other websites hosted by Dyalog Ltd.
-              In order to access dyalog.run directly you must agree to the
-              <a href="#terms-of-use">Terms of Use</a>.
+              In order to access dyalog.run directly you must agree to the <a href="#terms-of-use"
+              className="underline text-blue-500">Terms of Use</a>.
             </p>
           </article>
           <article id="privacy-policy">
@@ -68,32 +60,27 @@ export default function Home() {
               shared with anyone
             </p>
             <p className="text-justify mb-3">
-              See also the general Dyalog
-              <a href="https://www.dyalog.com/index.php?page=privacy-policy">
-                Privacy Policy
-              </a>.
+              See also the general Dyalog <a href="https://www.dyalog.com/index.php?page=privacy-policy"
+              className="underline text-blue-500">Privacy Policy</a>.
             </p>
           </article>
           <hr className="border-gray-400 dark:border-gray-700 my-4" />
           <article id="terms-of-use">
             <h1 className="mb-4 text-4xl font-bold">Terms of Use</h1>
             <p className="text-justify mb-3">
-            <strong>"The Service"</strong> refers to the website at
-            <a href="https://dyalog.run">https://dyalog.run</a>
-            and all services provided at that URL. This does not include the Software itself, but only the
-            instance of the Software made available at
-            <a href="https://dyalog.run">https://dyalog.run</a>.
+              <strong>"The Service"</strong> refers to the website at <a href="https://dyalog.run"
+              className="underline text-blue-500">https://dyalog.run</a> and all services provided at that URL.
+              This does not include the Software itself, but only the instance of the Software made available
+              at <a href="https://dyalog.run" className="underline text-blue-500">https://dyalog.run</a>.
             </p>
             <p className="text-justify mb-3">
-              Unless Dyalog Ltd grants you explicit written or electronic permission, you must not use
-              <strong>the Service</strong>, except directly through the web page and as a result of your
-              manual user interaction with it.
+              Unless Dyalog Ltd grants you explicit written or electronic permission, you must not use <strong>the
+                Service</strong>, except directly through the web page and as a result of your manual user interaction with it.
             </p>
             <p className="text-justify mb-3">
-              The Software includes an "API", which is a programmatic interface for interacting with
-              <strong>the Service</strong>, but this is for use only by
-              <strong>the Service</strong> internally, and by other websites hosted by Dyalog Ltd.
-              You are not permitted to use the API otherwise.
+              The Software includes an "API", which is a programmatic interface for interacting with <strong>the
+                Service</strong>, but this is for use only by <strong>the Service</strong> internally, and by
+                other websites hosted by Dyalog Ltd. You are not permitted to use the API otherwise.
             </p>
           </article>
         </main>
